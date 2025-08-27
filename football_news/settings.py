53201309e07c542931c7529fe89d42ac8a3c45e2
grdph123 <garugadph@gmail.com>
2025-08-27 21:00:45 +0700

    Update settings.py

--- football_news/settings.py
+++ football_news/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
