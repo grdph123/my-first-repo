fbad2e0d0e7afb21c03c9c71e58715e063ea8013
Garuga Dewangga Putra Handikto <garugadph@gmail.com>
2025-09-03 19:39:07 +0700

    Complete tutorial 1: Django MVT implementation

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406437615',
+        'name': 'Garuga Dewangga Putra Handikto',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
+
